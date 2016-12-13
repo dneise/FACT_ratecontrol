4b22e399687fab16da645abadaa2ec8571c1ed55
Dominik Neise <neised@phys.ethz.ch>
2016-12-13 14:03:12 +0100

    print thresholds

--- ratecontrol.cc
+++ ratecontrol.cc
@@ -114,14 +114,14 @@ private:
         AppendToHistoricCurrents(bias_currents);
         bias_currents = GetMedianOfHistoricCurrents();
 
-        PrintArray(bias_currents);
         auto thresholds = CalcThresholdsFromCurrents(bias_currents);
+        PrintArray(thresholds);
         auto replaced = ReplaceBrokenBiasPatches(thresholds);
         auto sorted_v = SortThresholdsIntoDualTriggerPatchOrder(replaced, fMap);
         auto proposed_thresholds = CombineThresholds(sorted_v);
 
         auto new_thresholds = SelectSignificantChanges(proposed_thresholds);
-        Out() << "bias_currents.size()" << bias_currents.size() << endl;
+
         PrintThresholdsOutOfRange(proposed_thresholds);
         if (GetCurrentState() == RateControl::State::kInProgress){
             SetThresholds(new_thresholds);
